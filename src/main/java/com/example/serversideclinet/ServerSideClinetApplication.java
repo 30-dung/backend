a6fb7a822f2620435d3b2560bd6389310604c91a
Vũ <vunnth2307024@fpt.edu.vn>
2025-03-23 12:57:43 +0700

    13/03/2025

---
+++ src/main/java/com/example/serversideclinet/ServerSideClinetApplication.java
@@ -0,0 +1,13 @@
+package com.example.serversideclinet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerSideClinetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerSideClinetApplication.class, args);
+    }
+
+}
